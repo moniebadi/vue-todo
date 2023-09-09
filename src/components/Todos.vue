b6fc2ed4a73e727676c10b0212b09188bb614913
Monica <moni.ebadi@gmail.com>
2023-09-10 02:25:23 +0430

    create todos components

---
+++ src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div :key="todo.id" v-for="todo in todos">
+            <TodoItem :todo="todo" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue'
+
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
