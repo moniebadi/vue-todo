8b45fa5ee287ee53582f840223571ebdb90bb165
monire ebadi <moni.ebadi@gmail.com>
2023-09-12 18:41:58 +0330

    update server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    host: '153.office.arjavand.local'
+  }
 });
